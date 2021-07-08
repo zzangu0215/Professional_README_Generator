bb19989cebb01b20b86a70cf7f36db1205ac7d43
Junny <tajo0215@gmail.com>
2021-07-07 18:38:47 -0700

    renderLicenseBadge function implemented

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,12 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === '') {
+    return;
+  } else {
+    return `![badge](https://img.shields.io/badge/license-${license}-brightgreen)  `;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,9 +18,16 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
 
-`;
+  const readmeBlock = `
+  
+  # ${data.title}
+
+
+  `
+
+
+  return readmeBlock;
 }
 
 module.exports = generateMarkdown;
